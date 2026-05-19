1082a7c37fb38c3ddc057c0d4d04c1984c277e44
helosvflx <heloisasevf@gmail.com>
2026-05-19 14:34:58 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     nome = "Helô"
-    return render_template('index.html', title='Página Inicial', usuario=None, 
-    nome=None, title= 'Home')
+    return render_template('index.html')
 
 
 @app.route('/usuario')
@@ -47,6 +46,54 @@ def arearestrita(id):
     else:
         return "Acesso permitido (cadeado aberto)"
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/alunos')
+def alunos():
+    lista_alunos = [
+        {'nome': 'Alice', 'matrícula': '12345678'},
+        {'nome': 'Bruno', 'matrícula': '98765432'},
+        {'nome': 'Clara', 'matrícula': '45678912'},
+        {'nome': 'Marcos', 'matrícula': '74125896'},
+        {'nome': 'Valéria', 'matrícula': '85236974'}
+    ]
+
+    return render_template('alunos.html', alunos=lista_alunos)
+
+
+@app.route('/')
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
+
+@app.route('/')
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+
+@app.route('/cardapio')
+def cardapio():
+    return render_template('cardapio.html')
+
+
+@app.route('/pedidos')
+def pedidos():
+    return render_template('pedidos.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/cliente/<nome>/lanche/<sabor>')
+def cliente(nome, sabor):
+    return render_template('pagdinamica.html', nome=nome, sabor=sabor)
 
 if __name__ == '__main__':
     app.run(debug=True)
